8285e76ff29613433e13ae9bd39106c4d51d2e67
Alex Leandro <alexorcizamus@gmail.com>
2023-11-08 13:58:23 -0300

    feat: copy to clipboard :floppy_disk:

--- src/components/Color.jsx
+++ src/components/Color.jsx
@@ -4,18 +4,31 @@ import Clipboard from "../assets/clipboard.svg?react";
 import styles from "./Color.module.css";
 
 const Color = ({ hex }) => {
+  const btn = React.useRef();
   const [copied, setCopied] = React.useState(false);
 
   function handleClick() {
     setCopied(true);
+    copyToClipboard(hex);
+
     setTimeout(() => {
       setCopied(false);
-    }, 2000);
+      // react useRef performed better than just using ev.target in this logic
+      btn.current.blur();
+    }, 750);
+  }
+
+  async function copyToClipboard(code) {
+    try {
+      await navigator.clipboard.writeText(code);
+    } catch (error) {
+      console.error("Failed to copy: ", error);
+    }
   }
 
   return (
     <li className={styles.color}>
-      <button style={{ backgroundColor: hex }} onClick={handleClick}>
+      <button style={{ backgroundColor: hex }} onClick={handleClick} ref={btn}>
         <span className={styles.copy}>
           {copied ? <ClipboardCheck /> : <Clipboard />}
         </span>
